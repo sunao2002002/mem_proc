c7ce3439f5cb8210d9fbbc4af70764988f0fb123
sun.ao <sunao2002002@gmail.com>
2017-01-03 09:50:13 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+set -e 
+PWD=$(pwd)
+BUILD_DIR=${PWD}/output
+
+if [   -e "$BUILD_DIR" ];then
+    rm -rf "$BUILD_DIR"
+fi
+mkdir -p "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+cmake ..
+make 
